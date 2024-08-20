e726bf357a863da6e2dee892f0f3dee03c872581
Oliv751 <olivierlopez751@gmail.com>
2024-08-20 20:09:12 +0200

    create alert on launch and for pikachu

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,58 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-import PokemonCard from "./components/PokemonCard"
+import PokemonCard from "./components/PokemonCard";
 import NavBar from "./components/NavBar";
 
-import "./App.css"
+import "./App.css";
 
 const pokemonList = [
   {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "charmander",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
-    },
-    {
-      name: "squirtle",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
-    },
-    {
-      name: "pikachu",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
-
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 function App() {
-  const [pokemonIndex,setPokemonIndex] = useState(0);
+  useEffect(() => {
+    alert("hello pokemon trainer :)");
+  }, []);
 
-  const handlePokemonSelect  = (index) =>{
-    setPokemonIndex(index)
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
+  const handlePokemonSelect = (index) => {
+    setPokemonIndex(index);
   };
-  
 
   return (
     <>
-     <div>
-      <PokemonCard pokemon={pokemonList[pokemonIndex]}/>
-      <NavBar
-      pokemonList={pokemonList}
-      onPokemonClick={handlePokemonSelect}
-      />
-     </div>
+      <div>
+        <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+        <NavBar
+          pokemonList={pokemonList}
+          onPokemonClick={handlePokemonSelect}
+        />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,23 +1,33 @@
 import PropTypes from "prop-types";
 
-function NavBar({pokemonList, onPokemonClick}) {
+function NavBar({ pokemonList, onPokemonClick }) {
   return (
     <nav>
-      {pokemonList.map((pokemon, index)=> (
-        <button key={pokemon.name} onClick={()=>onPokemonClick(index)}>{pokemon.name}</button>
+      {pokemonList.map((pokemon, index) => (
+        <button
+          key={pokemon.name}
+          onClick={() => {
+            if (pokemon.name.toLowerCase() === "pikachu") {
+              alert("Pika Pikachu !!!");
+            }
+            onPokemonClick(index);
+          }}
+        >
+          {pokemon.name}
+        </button>
       ))}
     </nav>
   );
 }
 
 NavBar.propTypes = {
-    pokemonList : PropTypes.arrayOf(
-        PropTypes.shape({
-            name: PropTypes.string.isRequired,
-            imgSrc: PropTypes.string,
-          })
-        ).isRequired,
-    onPokemonClick : PropTypes.func.isRequired,
+  pokemonList: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      imgSrc: PropTypes.string,
+    })
+  ).isRequired,
+  onPokemonClick: PropTypes.func.isRequired,
 };
 
 export default NavBar;
